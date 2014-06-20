9499333eacc7e6d223c495131447a58b3520075a
bpowell415 <bpowell415@gmail.com>
2014-06-20 08:25:41 -0500

    add in an app.js

---
+++ src/Breeze.BusinessTime.WebExample/Scripts/App/App.js
@@ -0,0 +1,16 @@
+﻿var App = App || 
+    
+(function() {
+    var app = {};
+
+    // insert js functionality here
+    app.run = function() {
+        
+    };
+
+    return app;
+}());
+
+$(function() {
+    App.run();
+})
